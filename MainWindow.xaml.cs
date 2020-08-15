973f0e4bbab6d446ae74c07146b47bb3f7543960
hwang <heidi.wang@advent.com>
2020-08-15 10:27:22 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ElecProj
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        List<short> listVolt = new List<short>();
+        List<double[]> listVoltPlot = new List<double[]>();
+        List<short> listCurrent = new List<short>();
+        List<double[]> listCurrentPlot = new List<double[]>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WebCurrent.Navigate(new Uri(System.IO.Directory.GetCurrentDirectory() + "/area.html"));
+            WebVolt.Navigate(new Uri(System.IO.Directory.GetCurrentDirectory() + "/area.html"));
+
+            initSerialPort();
+           // bt_stopSend.IsEnabled = false;
+        }
+
+        private void initSerialPort() {
+            //获取当前计算机的串行端口名的数组。
+            string[] ports = SerialPort.GetPortNames();
+            for (int index = 0; index < ports.Length; index++)
+            {
+                cb_SerialPortNumber.Items.Add(ports[index]);//添加item
+                cb_SerialPortNumber.SelectedIndex = index; //设置显示的item索引
+            }
+            bt_send.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// 打开发射机文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenTransmitterClick(object sender, RoutedEventArgs e)
+        {
+            listCurrent.Clear();
+            listCurrentPlot.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "文本文件|*.dat;*.DAT";
+            byte[] chrData = new byte[2];
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    while (fs.Read(chrData, 0, 2) > 0)
+                    {
+                        fs.Flush();
+                        //把两个字节转换为无符号整数
+                        listCurrent.Add(BitConverter.ToInt16(chrData, 0));
+                        if (listCurrent[listCurrent.Count - 1] == 32767 || listCurrent[listCurrent.Count - 1] == -32768)
+                        {
+                            listCurrent.RemoveAt(listCurrent.Count - 1);
+                        }
+                    }
+                    double offset = 0;
+                    for (int i = 10000; i < 18000; i += 40)
+                    {
+                        offset += listCurrent[i];
+                    }
+                    offset /= 200.0;
+                    for (int i = 0; i < listCurrent.Count; i += 4)
+                    {
+                        listCurrentPlot.Add(new double[] { i, (offset - listCurrent[i]) / 160.0 });
+                    }
+                    string yAxis = JsonConvert.SerializeObject(listCurrentPlot);
+                    WebCurrent.InvokeScript("jsPushData", yAxis);
+                }
+            }
+        }
+    
+
+        /// <summary>
+        /// 打开接收机文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenReceiverClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 计算装置系数K
+        /// </summary>
+        /// <param name="AM"></param>
+        /// <param name="AN"></param>
+        /// <param name="BM"></param>
+        /// <param name="BN"></param>
+        /// <returns></returns>
+        private double CentralGradientK(double AM, double AN, double BM, double BN)
+        {
+            return 2 * Math.PI * AM * AN * BM * BN / (AN - AM) / (AM * AN - BM * BN);
+        }
+
+      
+    }
+}
---
+++ SerialPortItem.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ElecProj
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SerialPort _serialPort = new SerialPort();
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void initialize()
+        {
+            //关闭串口时回抛异常
+            try
+            {
+                _serialPort.PortName = cb_SerialPortNumber.SelectedItem.ToString();//串口号
+                ComboBoxItem seletedItem = (ComboBoxItem)this.cb_BaudRate.SelectedItem;
+                _serialPort.BaudRate = Convert.ToInt32(seletedItem.Content.ToString());//波特率
+                _serialPort.DataBits = 8;//数据位
+                _serialPort.StopBits = StopBits.One;//停止位
+                _serialPort.Parity = Parity.None;//校验位
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// /串口开关
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_SerialSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            initialize();
+            string strContent = this.bt_SerialSwitch.Content.ToString();
+            if (strContent == "打开串口")
+            {
+                try
+                {
+                    _serialPort.Open();
+                    _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);//添加数据接收事件
+                    //_serialPort.DataReceived += DataReceivedHandler;
+                    bt_SerialSwitch.Content = "关闭串口";
+                    tb_switchStatus.Text = "串口状态 ";
+                    bt_send.IsEnabled = true;
+                    // bt_stopSend.IsEnabled = true;
+                    e_status.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0000"));
+                }
+                catch { }
+            }
+            else
+            {
+                try
+                {
+                    _serialPort.DataReceived -= DataReceivedHandler;
+                    _serialPort.Close();
+                    bt_SerialSwitch.Content = "打开串口";
+                    tb_switchStatus.Text = "串口状态 ";
+                    e_status.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+                    bt_send.IsEnabled = false;
+                    //bt_stopSend.IsEnabled = false;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 接收数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            int len = this._serialPort.BytesToRead;
+            byte[] buffer = new byte[len];
+            this._serialPort.Read(buffer, 0, len);
+            string strData = BitConverter.ToString(buffer, 0, len);
+            Dispatcher.Invoke(() =>
+            {
+                this.tb_receiveData.Text += strData;
+                this.tb_receiveData.Text += "-";
+            });
+        }
+
+
+        private void bt_send_Click(object sender, RoutedEventArgs e)//发送按钮
+        {
+            string SendData = tb_SendData.Text;
+            byte[] Data = new byte[20];
+            for (int i = 0; i < SendData.Length / 2; i++)
+            {
+                //每次取两位字符组成一个16进制
+                Data[i] = Convert.ToByte(tb_SendData.Text.Substring(i * 2, 2), 16);
+            }
+            this._serialPort.Write(Data, 0, Data.Length);
+        }
+       
+        /// <summary>
+        /// 清空接受数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearReceiveData_Click(object sender, RoutedEventArgs e)
+        {
+            tb_receiveData.Text = "";
+        }
+    }
+}
